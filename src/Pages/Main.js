1a435ada62624104a3ba55ef0fe889262529ba29
Elul-Asfaha <69459338+Elul-Asfaha@users.noreply.github.com>
2023-04-17 18:09:24 +0300

    Add files via upload

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { useState } from "react"
-import Showmore from "../components/Showmore"
+// import { useState } from "react"
+// import Showmore from "../components/Showmore"
 import data from '../mockdata/MOCK_DATA'
 import Card from '../components/Card'
 
@@ -8,18 +8,19 @@ import Card from '../components/Card'
 
 
 const Main=()=>{
-    const [amount,setAmount]=useState(18)
-    const handleAmountDisplayed=()=>{
-        setAmount(amount+12)
-    }
-    
+    // const [amount,setAmount]=useState(18)
+    // const handleAmountDisplayed=()=>{
+    //     setAmount(amount+12)
+    // } 
+    // this function is used to determine how many properties need to be displayed.
+    // the above funtion is currently disabled
     const dispData=data.map(items=><Card key={items.id} data={items} />)
     return(
             <div className="min-h-screen py-5 md:px-[5%]  md:pb-[13%] lg:pb-[5%] relative">
-                <div className="md:grid md:grid-cols-3 lg:grid-cols-6 gap-5">
+                <div className="flex flex-col md:grid md:grid-cols-3 lg:grid-cols-6 gap-5">
                     {dispData}
                 </div>
-                <Showmore AmountDisplayed={()=>handleAmountDisplayed()}/> 
+                {/* <Showmore AmountDisplayed={()=>handleAmountDisplayed()}/>  */}
             </div>
 
     )
--- src/components/Card.js
+++ src/components/Card.js
@@ -11,29 +11,31 @@ const Card=(props)=>{
     return(
         <div className='relative flex flex-col w-[100%]'>
             <Link to="/Details" onClick={handleShowDetails} className="flex flex-col md:max-w-[300px] justify-between h-[350px]">
-                <img src={props.data.Image} loading="lazy"  className='container rounded-lg mb-1 min-h-[65%] skeleton' alt=''/>
+                <div className='rounded-lg mb-1 min-h-[65%] skeleton'>
+                    <img src={props.data.Image} loading="lazy"  className='container' alt=''/>
+                </div>
                 <div className='flex justify-between flex-wrap'>
-                    <p className="font-bold">
+                    <p className="font-bold skeleton">
                         {props.data.City},
                         <span>
                              {' '}
                             {props.data.Country}
                         </span>
                     </p>
-                    <p className='flex gap-1'>
+                    <p className='flex gap-1 skeleton'>
                         <StarIcon/>
                         <span>
                             {props.data.Rating}
                         </span>
                     </p>
                 </div>
-                <p className='skeleton'>
+                <p className='skeleton w-full'>
                     Hosted by {props.data.Name}
                 </p>
-                <p>
+                <p className='skeleton'>
                     {props.data.Date}
                 </p>
-                <p className='font-semibold'><span>${props.data.Price}</span> Night</p>
+                <p className='font-semibold skeleton'><span>${props.data.Price}</span> Night</p>
                 
             </Link>
             <div className='absolute right-3 top-3 max-h-[30px] max-w-[30px] cursor-pointer'>
