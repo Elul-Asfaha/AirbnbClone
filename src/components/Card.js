178bd6d6a320a63686c25845e4bd6a98f9aa045b
Elul-Asfaha <69459338+Elul-Asfaha@users.noreply.github.com>
2023-04-17 20:44:35 +0300

    Add files via upload

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,29 +11,29 @@ const Card=(props)=>{
     return(
         <div className='relative flex flex-col w-[100%]'>
             <Link to="/Details" onClick={handleShowDetails} className="flex flex-col md:max-w-[300px] justify-between h-[350px]">
-                <img src={props.data.Image} loading="lazy"  className='container rounded-lg mb-1 min-h-[65%] skeleton' alt=''/>
+                <img src={props.data.Image && props.data.Image} loading="lazy"  className='container rounded-lg mb-1 min-h-[65%] skeleton' alt=''/>
                 <div className='flex justify-between flex-wrap'>
                     <p className="font-bold skeleton">
-                        {props.data.City},
+                        {props.data.City && props.data.City},
                         <span>
                              {' '}
-                            {props.data.Country}
+                            {props.data.Country && props.data.Country}
                         </span>
                     </p>
                     <p className='flex gap-1 skeleton'>
                         <StarIcon/>
                         <span>
-                            {props.data.Rating}
+                            {props.data.Rating && props.data.Rating}
                         </span>
                     </p>
                 </div>
                 <p className='skeleton w-full'>
-                    Hosted by {props.data.Name}
+                    Hosted by {props.data.Name && props.data.Name}
                 </p>
                 <p className='skeleton'>
-                    {props.data.Date}
+                    {props.data.Date && props.data.Date}
                 </p>
-                <p className='font-semibold skeleton'><span>${props.data.Price}</span> Night</p>
+                <p className='font-semibold skeleton'><span>${props.data.Price && props.data.Price}</span> Night</p>
                 
             </Link>
             <div className='absolute right-3 top-3 max-h-[30px] max-w-[30px] cursor-pointer'>
